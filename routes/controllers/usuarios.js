d662b3aa8b4c8b9e1db94e5e950e24183477ddc2
Huguez <carlos.huguez@gmail.com>
2020-10-19 19:13:20 -0700

    validar que el correo electronico sea unico

--- routes/controllers/usuarios.js
+++ routes/controllers/usuarios.js
@@ -1,5 +1,8 @@
 const Usuario = require('../../models/usuario');
 
+const { response } = require('express');
+
+
 
 const getUsuarios =  async ( req, res ) => {
 
@@ -11,18 +14,34 @@ const getUsuarios =  async ( req, res ) => {
     } );
 }
 
-const postUsuarios = async ( req, res ) => {
+const postUsuarios = async ( req, res = response ) => {
 
     const { nombre, email, password }  = req.body;
+    
+    try {
+        
+        const existeUsuario = await Usuario.findOne({ email });
 
-    const usuario = new Usuario( { nombre, email, password } );
+        if( existeUsuario ){
+            return res.status( 400 ).json({ ok: false, msj: "El correo ya esta registrado!!!" });
+        }
 
-    await usuario.save();
-
-    res.json( { 
-        ok: true, 
-        usuario
-    } );
+        const usuario = new Usuario( { nombre, email, password } );
+        
+        await usuario.save();
+    
+        res.json( { 
+            ok: true, 
+            usuario
+        } );
+        
+    } catch (error) {
+        console.log(error);
+        res.status( 500 ).json({
+            ok:false,
+            msj: "Error inesperado!!!"
+        });
+    }
 }
 
 
