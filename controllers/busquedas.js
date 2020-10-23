dbd4f21bc26e39b21f959f40551e794de91a52bb
Huguez <carlos.huguez@gmail.com>
2020-10-22 18:23:48 -0700

    Busqueda por coleccion

--- controllers/busquedas.js
+++ controllers/busquedas.js
@@ -4,6 +4,7 @@ const { response } = require('express');
 const Usuarios = require('../models/usuario');
 const Medicos = require('../models/medicos');
 const Hospital = require('../models/hospital');
+const { collection } = require('../models/usuario');
 
 const getAll = async ( req, res = response ) => {
     try {
@@ -31,4 +32,49 @@ const getAll = async ( req, res = response ) => {
     }
 }
 
-module.exports = { getAll };
+
+const getDocumentosColeccion = async ( req, res = response ) => {
+    try {
+        const busqueda = req.params.busqueda;
+        const coleccion = req.params.tabla;
+        const regex = new RegExp( busqueda, 'i' );
+        
+        let result;
+        switch( coleccion ){
+            case 'medicos':
+                result = await Medicos.find( { nombre: regex } ).populate( 'usuario', ' nombre img' ).populate( 'hospital', ' nombre img' );
+    
+                break;
+            case 'usuarios':
+                result = await Usuarios.find( { nombre: regex } ).populate( 'usuario', ' nombre img' );
+                
+                break;
+            case 'hospitales':
+                result = await Hospital.find( { nombre: regex } );
+                
+                break;
+            default:
+                res.status( 400 ).json({
+                    ok: false,
+                    msg: "no se encontro nada la busqueda debe de ser medicos/usuarios/hospitales"
+                });
+        }
+
+
+        res.json({
+            ok: true,
+            result
+        });
+
+    } catch (error) {
+        res.status( 500 ).json({
+            ok: false,
+            msg: "Error en el server!!!"
+        });
+    }
+}
+
+module.exports = { 
+    getAll,
+    getDocumentosColeccion
+};
--- routes/busquedas.js
+++ routes/busquedas.js
@@ -1,13 +1,13 @@
-// path: api/todo /:busqueda
+// path: api/todo
 
 const { Router } = require('express');
 const { validarJWT } = require('../middleware/validar-jwt');
-const { getAll } = require('../controllers/busquedas');
+const { getAll, getDocumentosColeccion } = require('../controllers/busquedas');
 const router = Router();
 
-router.get( '/:busqueda', [
-    validarJWT
-], getAll );
+router.get( '/:busqueda', validarJWT, getAll );
+
+router.get( '/coleccion/:tabla/:busqueda', validarJWT, getDocumentosColeccion );
 
 
 module.exports = router;
