4cd5ca9cfd6db35850e0c37535b946fd7f0491e3
Huguez <carlos.huguez@gmail.com>
2020-10-27 16:08:35 -0700

    crear una aplicacion rapida para probar el Google Sing-in

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,6 +4,8 @@ const Usuario = require('../models/usuario');
 const bcrypt = require('bcryptjs') ;
 const { generarJWT } = require('../helpers/jwt');
 
+
+
 const login = async ( req, res = response ) => {
     try {
         const { email, password } = req.body;
--- index.js
+++ index.js
@@ -15,6 +15,9 @@ app.use( express.json() );
 //base de datos 
 dbConection();
 
+// directorio publico
+app.use( express.static( 'public' ) );
+
 // Rutas
 app.use( '/api/usuarios', require('./routes/usuarios') );
 app.use( '/api/login', require('./routes/auth') );
