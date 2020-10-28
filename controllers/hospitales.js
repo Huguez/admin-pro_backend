8b653d312484b9359c8a6c915b48ed2ee6d9112e
Huguez <carlos.huguez@gmail.com>
2020-10-28 01:26:10 -0700

    operaciones de editar y eliminar hospitales creada

--- controllers/hospitales.js
+++ controllers/hospitales.js
@@ -37,19 +37,69 @@ const crearHospital = async ( req, res = response ) => {
 }
 
 
-const actualizarHospital = ( req, res = response ) => {
-    res.json({
-        ok: true,
-        msg: "si se hizo!!!"
-    })
+const actualizarHospital = async ( req, res = response ) => {
+    try {
+        const id = req.params.id;
+        const userId = req.params.id;
+
+        const hospitalDB = await Hospital.findById( id );
+        
+        if( !hospitalDB ){
+            return res.status( 404 ).json( {
+                ok:false,
+                msg: 'No existe ese hospital'
+            } );
+        }
+        
+        const campos = { ...req.body, usuario: userId };
+
+        // const hospitalActualiado = await Hospital.findByIdAndUpdate( id, campos, { new: true } );
+        const hospitalActualiado = await Hospital.findOneAndUpdate( id, campos, { new: true } );
+
+        res.json({
+            ok: true,
+            msg: "si se hizo!!!",
+            hospital: hospitalActualiado
+        });
+
+    } catch (error) {
+        res.status( 500 ).json( {
+            ok: false,
+            msg: "error de Server!!!!"
+        });
+    }
 }
 
 
-const borrarHospital = ( req, res = response ) => {
-    res.json({
-        ok: true,
-        msg: "si se hizo!!!"
-    })
+const borrarHospital = async ( req, res = response ) => {
+    try {
+        const id = req.params.id;
+        console.log( id );
+        
+        const hospitalDB = await Hospital.findById( id );
+
+        if( !hospitalDB ){
+            return res.status( 404 ).json( {
+                ok:false,
+                msg: 'No existe ese usuario'
+            } );            
+        }
+
+        const hospitalBorrado = await Hospital.findByIdAndDelete( id );
+
+        res.json({
+            ok: true,
+            hospitalBorrado
+        });
+
+
+
+    }catch( error ) {
+        res.status( 500 ).json({
+            ok: true,
+            msg: "Valio queso!!!"
+        });   
+    }
 }
 
 module.exports = {
