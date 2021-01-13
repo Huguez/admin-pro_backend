57e4be1040d3e9f92200d98fa28e050880897ec1
Huguez <carlos.huguez@gmail.com>
2021-01-12 21:44:56 -0700

    Backend - Servir una SPA

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const { dbConection } = require('./database/config');
 const express = require("express");
 const cors = require('cors'); 
+const path = require('path');
 
 // crear el servidor de express
 const app = express();
@@ -26,6 +27,11 @@ app.use( '/api/medicos', require('./routes/medicos') );
 app.use( '/api/todo', require('./routes/busquedas') );
 app.use( '/api/upload', require('./routes/uploads') );
 
+
+// lo ultimo
+app.get('*', (rep, res ) => { 
+    res.sendFile( path.resolve(__dirname, 'public/index.html') )
+});
 
 app.listen( process.env.PORT, () => {
     // console.log("Soy un wawa!!!");
